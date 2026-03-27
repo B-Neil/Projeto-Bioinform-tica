4a4165d4edeaeeef834d20d1dc4545f72d00020b
B_Neil <joaoferreira.ac23@gmail.com>
2026-03-27 13:35:03 -0400

    improved upimapi final results

--- Project/workflow/scripts/module_1/upimapi_results.py
+++ Project/workflow/scripts/module_1/upimapi_results.py
@@ -4,53 +4,75 @@ import sys
 import pandas as pd
 
 
-def build_extras(row, excluded_cols):
-    parts = []
-    for col, val in row.items():
-        if col in excluded_cols:
-            continue
-        if pd.notna(val) and str(val).strip() != "":
-            parts.append(f"{col}: {val}")
-    return "; ".join(parts)
+def is_empty(val):
+    return pd.isna(val) or str(val).strip() == ""
+
+
+# def build_extras(row, excluded_cols):
+#     parts = []
+#     for col, val in row.items():
+#         if col in excluded_cols:
+#             continue
+#         if not is_empty(val):
+#             parts.append(f"{col}: {val}")
+#     return "; ".join(parts)
+
+
+def should_remove(row):
+    protein_name = str(row["Protein names"])
+
+    cond_protein = "Uncharacterized protein" in protein_name
+
+    cond_empty_info = (
+        is_empty(row["Function [CC]"]) and
+        is_empty(row["Gene Ontology (GO)"]) and
+        is_empty(row["EC number"]) and
+        is_empty(row["Pfam"])
+    )
+
+    return cond_protein and cond_empty_info
 
 
 def main(input_tsv, output_tsv):
     df = pd.read_csv(input_tsv, sep="\t")
 
+    # Colunas a manter agora inclui Pfam
     keep_cols = [
         "qseqid",
         "Protein names",
         "Gene Ontology (GO)",
         "EC number",
-        "Function [CC]",
+        "Pfam",
     ]
 
     missing = [c for c in keep_cols if c not in df.columns]
     if missing:
         raise ValueError(f"Colunas em falta no ficheiro: {missing}")
 
+    # -------- FILTRO --------
+    df = df[~df.apply(should_remove, axis=1)].copy()
+
     excluded = set(keep_cols)
 
-    # Criar coluna extra
-    df["extra"] = df.apply(lambda r: build_extras(r, excluded), axis=1)
+    # # Criar extra
+    # df["extra"] = df.apply(lambda r: build_extras(r, excluded), axis=1)
 
-    # Criar novo dataframe
+    # Novo dataframe
     new_df = df[keep_cols].copy()
-    new_df["extra"] = df["extra"]
+    # new_df["extra"] = df["extra"]
 
-    # Adicionar coluna tool
+    # Adicionar tool
     new_df["tool"] = "UPIMAPI"
 
-    # ORDEM FINAL DAS COLUNAS
+    # Ordem final das colunas
     new_df = new_df[
         [
             "qseqid",
             "tool",
             "Protein names",
-            "Function [CC]",
             "Gene Ontology (GO)",
             "EC number",
-            "extra",
+            "Pfam",
         ]
     ]
 
@@ -63,7 +85,4 @@ if __name__ == "__main__":
         print("  python extract_upimapi.py input.tsv output.tsv")
         sys.exit(1)
 
-    input_tsv = sys.argv[1]
-    output_tsv = sys.argv[2]
-
-    main(input_tsv, output_tsv)
+    main(sys.argv[1], sys.argv[2])
